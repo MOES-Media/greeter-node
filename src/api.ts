42e7e878a04f93138dc6f4633b5e8313085d6231
Jeroen Nouws <jeroen.nouws@lemon.be>
2023-01-31 01:01:25 +0100

    feat(greet): added support for post operation, using name in request body to send over gRPC

--- src/api.ts
+++ src/api.ts
@@ -2,14 +2,14 @@ import {Request, Response} from "express";
 import {GreetApiServiceClient} from "@moes-media/resources/be/moesmedia/resources/greet/v1/greet_api_grpc_pb.js";
 import {ChannelCredentials} from "@grpc/grpc-js";
 import messages from "@moes-media/resources/be/moesmedia/resources/greet/v1/greet_pb.js";
+import {GetGreeting200Response, GetGreetingRequest} from "@moes-media/greeter-api";
 
 const greeterApiClient = new GreetApiServiceClient("localhost:6767", ChannelCredentials.createInsecure())
 
-export const getGreeting = (req: Request<{ body: { name?: string } }>, resp: Response) => {
-    console.log("get greeting")
-    const name = "General Kenobi";
-    greeterApiClient.getGreeting(new messages.GetGreetingRequest().setName(name), (err, grpcResp) => {
+export const getGreeting = (req: Request<GetGreetingRequest>, resp: Response<GetGreeting200Response>) => {
+    console.log(req.body)
+    greeterApiClient.getGreeting(new messages.GetGreetingRequest().setName(req.body.name), (err, grpcResp) => {
         resp.status(200)
-        resp.json({greeting: grpcResp.getGreeting()})
+            .json({greeting: grpcResp.getGreeting()})
     })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express, {Express, Request, Response} from "express";
-import {getGreeting} from "./api";
+import * as api from "./api";
+import swaggerRoutes from "swagger-routes-express"
+import apiDefinition from "@moes-media/greeter-api"
+import bodyParser from "body-parser"
 
 const app: Express = express()
 const port = process.env.PORT || 3000
 
-app.get("/greeting", getGreeting)
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+const connect = swaggerRoutes.connector(api as any, apiDefinition)
+connect(app)
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
